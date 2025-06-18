af034e76e1fbe7e7c7bc9dc61b2b1bcebd5846c4
Aranya5 <aranyabasu5@gmail.com>
2025-06-18 23:12:39 +0530

    Calculator

--- Java Script/Cousera/new.js
+++ Java Script/Cousera/new.js
@@ -14,3 +14,12 @@ var result1 = (test1 == 0);
 var result2 = (test2 == 0);
 console.log("Is "+num1+" an even number? " +result1);
 console.log("Is "+num2+" an even number? " +result2);
+
+// style with loops
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
