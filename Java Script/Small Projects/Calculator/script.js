8722a75e504fd3c7c9c68b7d04a0bf446893e54c
Aranya5 <aranyabasu5@gmail.com>
2025-06-20 23:24:09 +0530

    Calculator with basic operators

--- Java Script/Small Projects/Calculator/script.js
+++ Java Script/Small Projects/Calculator/script.js
@@ -1,4 +1,4 @@
-let input = document.getElementById('input');
+let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 let string = "";
@@ -11,6 +11,10 @@ arr.forEach(button => {
     }
     else if (e.target.innerHTML == "AC") {
       string = "";
+      input.value = string;
+    }
+    else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
       input.value = string;
     }
     else{
