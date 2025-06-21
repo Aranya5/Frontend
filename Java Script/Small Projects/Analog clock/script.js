45f2532239dbbf47d2e6a121cba17b2d507e65b5
Aranya5 <aranyabasu5@gmail.com>
2025-06-22 00:20:38 +0530

    Full Working Analog clock

--- Java Script/Small Projects/Analog clock/script.js
+++ Java Script/Small Projects/Analog clock/script.js
@@ -1,5 +1,20 @@
-let hr = document.getelementById("hour");
+let hr = document.getElementById("hour");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-function sisplayTime()
+function displayTime(){
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  let hRotation = 30 * hh + mm / 2;
+  let mRotation = 6 * mm + ss / 10;
+  let sRotation = 6 * ss;
+
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+setInterval(displayTime, 1000);
