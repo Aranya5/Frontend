df9a119989dadc4df34cfc4fb127a98dabe36232
Aranya5 <aranyabasu5@gmail.com>
2025-07-01 23:34:25 +0530

    Memory Game

---
+++ Java Script/Small Projects/Memory Game/script.js
@@ -0,0 +1,17 @@
+const moves = document.getelemantById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById(".result");
+const controls = document.querySelector(".controls-container");
+
+let cards;
+let interval;
+let firstcard = false;
+let secondcard = false;
+
+//iteams array
+const item =[
+  {}
+]
